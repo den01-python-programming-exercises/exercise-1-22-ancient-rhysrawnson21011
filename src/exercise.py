f553a99b7314b039e316c5b8103026ef989bfccf
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 15:49:12 +0000

    ancient history

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
-
+  ancient_history =  2015
+    
+  while True:
+      guess = int(input('what is the ancient:'))
+      if guess > ancient_history:
+        print('not ancient')
+      if guess <= ancient_history: 
+        print('ancient ')
+        break
 if __name__ == '__main__':
     main()
